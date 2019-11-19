39c07e8f3249e074e2e1e09989a68bb6cb6c9e1e
anges-bai <45257069+anges-bai@users.noreply.github.com>
2019-11-19 21:12:10 +0800

    Create DeadLock.java

---
+++ DeadLock.java
@@ -0,0 +1,62 @@
+package com.bit.local;
+
+/**
+ * @Author: BaiMiao
+ * @Date: 2019/11/19 21:09
+ * @Description:
+ */
+class Pen {
+    private String pen = "笔";
+
+    public String getPen() {
+        return pen;
+    }
+}
+
+class Book {
+    private String book = "本";
+
+    public String getBook() {
+        return book;
+    }
+}
+
+public class DeadLock {
+    private static Pen pen = new Pen();
+    private static Book book = new Book();
+
+    public static void main(String[] args) {
+        new DeadLock().deadLock();
+    }
+
+    public void deadLock() {
+        Thread thread1 = new Thread(new Runnable() {
+            // 笔线程 
+            @Override
+            public void run() {
+                synchronized (pen) {
+                    System.out.println(Thread.currentThread() + " :我有笔，我就不给你");
+                    synchronized (book) {
+                        System.out.println(Thread.currentThread() + " :把你的本给我！");
+                    }
+                }
+            }
+        }, "Pen");
+        Thread thread2 = new Thread(new Runnable() {
+            //本子线程 
+            @Override
+            public void run() {
+                synchronized (book) {
+                    System.out.println(Thread.currentThread() + " :我有本子，我就不给你！");
+                    synchronized (pen) {
+                        System.out.println(Thread.currentThread() + " :把你的笔给我！");
+                    }
+                }
+            }
+        }, "Book");
+        thread1.start();
+        thread2.start();
+    }
+}
+
+       
