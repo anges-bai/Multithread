6b3d9ba38ddb0df803b675177cf18f407b2cfa77
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-02-09 20:59:39 +0800

    Create cachedThreadPool.java

---
+++ cachedThreadPool.java
@@ -0,0 +1,27 @@
+package bit.test;
+
+import java.util.concurrent.*;
+
+public class Test {
+    public static void main(String[] args) {
+        ExecutorService executorService = 
+                Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 0; j < 10; j++) {
+                        System.out.println(Thread.currentThread().
+                                getName() + "、" + j);
+                    }
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+}
